f4ea8f48a750d477072d8d327dc4837e249c3a82
Tessa Hayden <tessahayden3@gmail.com>
2021-10-12 16:48:29 -0700

    week 3 exercise

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,14 +7,16 @@ class CampsiteInfo extends Component {
     renderCampsite(campsite) {
         if (campsite) {
             return (
-                <div class="col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className="container">
+                    <div class="col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={campsite.image} alt={campsite.name} />
+                            <CardBody>
+                                <CardTitle>{campsite.name}</CardTitle>
+                                <CardText>{campsite.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
                 </div>
             );
         }
@@ -44,15 +46,14 @@ class CampsiteInfo extends Component {
     }
     render() {
         const campsite = this.props.campsite;
-
-        if (campsite) {
-            return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
-            );
-        }
+            if (this.props.campsite) {
+                return (
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                );
+            }
         return (
             <div />
         );
