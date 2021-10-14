58cd3694b105616f3b9fadc1ccde2a2c78576ab9
Tessa Hayden <tessahayden3@gmail.com>
2021-10-14 09:38:12 -0700

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,63 +1,58 @@
 
 import { classPrivateMethod } from '@babel/types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite) {
-        if (campsite) {
-            return (
-                <div className="container">
-                    <div class="col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={campsite.image} alt={campsite.name} />
-                            <CardBody>
-                                <CardTitle>{campsite.name}</CardTitle>
-                                <CardText>{campsite.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                </div>
-            );
-        }
-    }
-    renderComments(comments) {
-        if (comments) {
-            return (
-                <div class="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map((comment) => {
-                        return (
-                            <div key={comment.id}>
-                                <p>{comment.text} <br />
-                                    --{comment.author}, {" "}
-                                    {new Intl.DateTimeFormat("en-US", {
-                                        year: "numeric",
-                                        month: "short",
-                                        day: "2-digit",
-                                    }).format(new Date(Date.parse(comment.date)))}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            );
-        }
-    }
-    render() {
-        const campsite = this.props.campsite;
-            if (this.props.campsite) {
-                return (
-                    <div className="container">
-                        <div className="row">
-                            {this.renderCampsite(this.props.campsite)}
-                            {this.renderComments(this.props.campsite.comments)}
+function RenderCampsite({campsite}) {
+    return (
+        <div class="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+function RenderComments({ comments }) {
+    if (comments) {
+        return (
+            <div class="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map((comment) => {
+                    return (
+                        <div key={comment.id}>
+                            <p>{comment.text} <br />
+                                --{comment.author}, {" "}
+                                {new Intl.DateTimeFormat("en-US", {
+                                    year: "numeric",
+                                    month: "short",
+                                    day: "2-digit",
+                                }).format(new Date(Date.parse(comment.date)))}
+                            </p>
                         </div>
-                    </div>
-                );
-            }
-        return <div />;
+                    );
+                })}
+            </div>
+        );
     }
 }
+function CampsiteInfo(props) {
+    const campsite = props.campsite;
+        if (props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div>
+                </div>
+            );
+        }
+    return <div />;
+}
+
 
 export default CampsiteInfo;
