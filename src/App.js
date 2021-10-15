8ce72efb83317bbdec69318e9a52eb908c1aa0c8
Tessa Hayden <tessahayden3@gmail.com>
2021-10-14 17:40:16 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponents';
+import { BrowseRouter } from 'react-router-dom';
 import './App.css';
 
 
 class App extends Component {
   render() {
-    return (
+    <BrowserRouter>
       <div className="App">
         <Main />
       </div>
-    );
+    </BrowserRouter>
   }
 }
 
--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -4,14 +4,34 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+            campsites: CAMPSITES
         };
+    }
+    render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component="{HomePage}" />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        )
     }
 
     onCampsiteSelect(campsiteId) {
