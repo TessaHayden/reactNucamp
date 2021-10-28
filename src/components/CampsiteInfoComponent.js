22faf221ca4121d5a3d56bc48c4220bc55b16bad
Tessa Hayden <tessahayden3@gmail.com>
2021-10-28 00:54:22 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,7 +26,7 @@ class CommentForm extends Component {
     }
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
     render() {
         return (
@@ -89,7 +89,7 @@ function RenderCampsite({campsite}) {
         </div>
     );
 }
-function RenderComments({ comments, addComment, campsiteId}) {
+function RenderComments({ comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div class="col-md-5 m-1">
@@ -108,7 +108,7 @@ function RenderComments({ comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                <CommentForm campsiteId={campsiteId} postComment={postComment}/>
             </div>
         );
     }
@@ -142,7 +142,7 @@ function CampsiteInfo(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}/>
                     </div>
                 </div>
@@ -164,7 +164,7 @@ function CampsiteInfo(props) {
             <div className="row">
                 <RenderCampsite campsite={props.campsite} />
                 <RenderComments comments={props.comments} 
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     campsiteId={props.campsite.id}/>
             </div>
         </div>
